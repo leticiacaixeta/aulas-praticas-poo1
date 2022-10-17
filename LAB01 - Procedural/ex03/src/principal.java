d2afb7034f8567fd7cedfb7cef451aaa71980e62
leticiacaixeta <leticiacaixeta@yahoo.com.br>
2022-10-17 20:45:53 -0300

    Merge branch 'main' of https://github.com/btravencolo/gsi015-labs-2022-01 # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- LAB01 - Procedural/ex03/src/principal.java
+++ LAB01 - Procedural/ex03/src/principal.java
@@ -48,12 +48,79 @@ public class principal {
 
     public static void ex03() {
         Scanner sc = new Scanner(System.in);
-        // ....
+        int[] vet = new int[6];
+        System.out.println("<< Listando um vetor >>");
+
+        for (int i = 0; i < 6; i++){
+            System.out.printf("Entre com o número %d:\n",i+1);
+            vet[i] = sc.nextInt();
+        }
+
+        System.out.println("Os valores lidos são:");
+        for (int i = 5; i >= 0; i--){
+            System.out.printf("%d ",vet[i]);
+        }
+    }
+
+    public static void ex04(){
+        Scanner sc = new Scanner(System.in);
+        int[] vet = new int[6];
+        System.out.println("<< Listando um vetor de números pares>>");
+
+        int i = 0;
+        while(i<6){
+            System.out.printf("Entre com o número %d:\n",i+1);
+            vet[i] = sc.nextInt();
+
+            if(vet[i]%2==1){
+                System.out.printf("Valor inválido");
+                continue;
+            }
+            i++;
+        }
+
+        System.out.println("Os valores lidos são:");
+        for (i = 0; i < 6; i++){
+            System.out.printf("%d ",vet[i]);
+        }
+    }
+
+    public static void ex05(){
+        Scanner sc = new Scanner(System.in);
+        int[] vet = new int[6];
+        System.out.println("<< Listando um vetor >>");
+
+        for (int i = 0; i < 5; i++){
+            System.out.printf("Entre com o número %d:\n",i+1);
+            vet[i] = sc.nextInt();
+        }
+
+        System.out.println("Os valores lidos são:");
+        for (int i = 0; i < 5; i++){
+            System.out.printf("%d ",vet[i]);
+        }
+
+        int maior = vet[0];
+        int menor = vet[0];
+        for (int i = 0; i < 5; i++){
+            if(vet[i]>maior){
+                maior = vet[i];
+            }
+            if(vet[i]<menor){
+                menor = vet[i];
+            }
+        }
+        System.out.printf("O maior valor é %d:\n", maior);
+        System.out.printf("O menor valor é %d:\n", menor);
+
     }
 
     public static void main(String[] args) {
-        // ex01();
-        ex02();
+        //ex01();
+        //ex02();
+        //ex03();
+        //ex04();
+        ex05();
     }
 
 
