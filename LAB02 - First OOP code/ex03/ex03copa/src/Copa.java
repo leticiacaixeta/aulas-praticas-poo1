431a2acd029fa889d593205df2d58939f9fbd9fa
leticia <leticiacaixeta@yahoo.com.br>
2022-11-06 16:37:12 -0300

    commit

--- LAB02 - First OOP code/ex03/ex03copa/src/Copa.java
+++ LAB02 - First OOP code/ex03/ex03copa/src/Copa.java
@@ -9,6 +9,7 @@ informações de todos os jogadores. Utilize laços para manipulação do vetor.
 public class Copa {
     public static void main(String[] args){
         Figurinha[] figurinha = new Figurinha[3];
+
         figurinha[0] = new Figurinha("Pele",LocalDate.parse("1940-10-23"),1.73f,80,"Atacante","Brasil");
         figurinha[1] = new Figurinha("Romário",LocalDate.parse("1966-01-29"),1.68f,70,"Atacante","Brasil");
         figurinha[2] = new Figurinha("Neymar",LocalDate.parse("1992-02-05"),1.75f,68,"Atacante","Brasil");
